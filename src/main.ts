733368d0b3b537e4a164ed2e140bdec5107db265
monsara <viktor.hirenko@gmail.com>
2025-02-24 14:08:26 +0200

    fix: prevent scrolling in ad iframes, remove unused files and folders

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,6 @@
-// import './assets/styles/main.scss'
 import '@/assets/styles/scss/main.scss'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
 import { createApp } from 'vue'
 import App from './App.vue'
-import i18n from "@/translations/i18n";
 
 const app = createApp(App)
-app.use(VueAxios, axios)
-app.provide('axios', app.config.globalProperties.axios)
-app.use(i18n)
 app.mount('#app')
